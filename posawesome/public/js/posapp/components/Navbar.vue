0984f77d6b2e799740d2a1af366d77471a80d422
Ahmed Osama <eng.ahmed.osama.1190@gmail.com>
2025-05-18 09:15:52 +0000

    chore: add comments and revamp

--- posawesome/public/js/posapp/components/Navbar.vue
+++ posawesome/public/js/posapp/components/Navbar.vue
@@ -1,21 +1,27 @@
 <template>
+  <!-- Main navigation container -->
   <nav>
-    <!-- Top App Bar -->
+    <!-- Top App Bar: application header with nav toggle, logo, title, and actions -->
     <v-app-bar app flat height="56" color="white" class="border-bottom">
-      <v-app-bar-nav-icon ref="navIcon" @click="drawer = !drawer" class="text-secondary" />
+      <!-- Nav icon: toggles side drawer -->
+      <v-app-bar-nav-icon ref="navIcon" @click="handleNavClick" class="text-secondary" />
 
+      <!-- Logo: POS Awesome brand image -->
       <v-img src="/assets/posawesome/js/posapp/components/pos/pos.png" alt="POS Awesome" max-width="32" class="mx-3" />
 
-      <v-toolbar-title @click="go_desk" class="text-h6 font-weight-bold text-primary" style="cursor: pointer;">
+      <!-- Title: clickable to navigate to desk view -->
+      <v-toolbar-title @click="goDesk" class="text-h6 font-weight-bold text-primary" style="cursor: pointer;">
         <span class="font-weight-light">POS</span><span>Awesome</span>
       </v-toolbar-title>
 
       <v-spacer />
 
+      <!-- Profile Button: displays current POS profile name -->
       <v-btn style="cursor: unset; text-transform: none;" variant="text" color="primary">
-        <span right>{{ pos_profile.name }}</span>
+        {{ posProfile.name }}
       </v-btn>
 
+      <!-- User Menu: dropdown for shift, print, logout, and about actions -->
       <v-menu offset-y offset-x :min-width="200">
         <template #activator="{ props }">
           <v-btn v-bind="props" color="primary" theme="dark" variant="text" class="user-menu-btn">
@@ -26,34 +32,26 @@
 
         <v-card class="user-menu-card" tile>
           <v-list dense class="user-menu-list">
-            <v-list-item v-if="!pos_profile.posa_hide_closing_shift" @click="close_shift_dialog" class="user-menu-item">
-              <v-list-item-icon>
-                <v-icon>mdi-content-save-move-outline</v-icon>
-              </v-list-item-icon>
+            <v-list-item v-if="!posProfile.posa_hide_closing_shift" @click="openCloseShift" class="user-menu-item">
+              <v-list-item-icon><v-icon>mdi-content-save-move-outline</v-icon></v-list-item-icon>
               <v-list-item-title>{{ __('Close Shift') }}</v-list-item-title>
             </v-list-item>
 
-            <v-list-item v-if="pos_profile.posa_allow_print_last_invoice && last_invoice" @click="print_last_invoice"
+            <v-list-item v-if="posProfile.posa_allow_print_last_invoice && lastInvoiceId" @click="printLastInvoice"
               class="user-menu-item">
-              <v-list-item-icon>
-                <v-icon>mdi-printer</v-icon>
-              </v-list-item-icon>
+              <v-list-item-icon><v-icon>mdi-printer</v-icon></v-list-item-icon>
               <v-list-item-title>{{ __('Print Last Invoice') }}</v-list-item-title>
             </v-list-item>
 
             <v-divider class="my-2" />
 
             <v-list-item @click="logOut" class="user-menu-item">
-              <v-list-item-icon>
-                <v-icon>mdi-logout</v-icon>
-              </v-list-item-icon>
+              <v-list-item-icon><v-icon>mdi-logout</v-icon></v-list-item-icon>
               <v-list-item-title>{{ __('Logout') }}</v-list-item-title>
             </v-list-item>
 
-            <v-list-item @click="go_about" class="user-menu-item">
-              <v-list-item-icon>
-                <v-icon>mdi-information-outline</v-icon>
-              </v-list-item-icon>
+            <v-list-item @click="goAbout" class="user-menu-item">
+              <v-list-item-icon><v-icon>mdi-information-outline</v-icon></v-list-item-icon>
               <v-list-item-title>{{ __('About') }}</v-list-item-title>
             </v-list-item>
           </v-list>
@@ -61,49 +59,41 @@
       </v-menu>
     </v-app-bar>
 
-    <!-- Navigation Drawer -->
+    <!-- Side Navigation Drawer -->
     <v-navigation-drawer app v-model="drawer" :mini-variant="mini" expand-on-hover width="220" class="drawer-custom"
-      @mouseleave="drawer = false; mini = true">
-      <!-- Drawer Header (expanded) -->
+      @mouseleave="handleMouseLeave">
+      <!-- Expanded Header -->
       <div v-if="!mini" class="drawer-header">
-        <v-avatar size="40">
-          <v-img :src="company_img" alt="Company logo" />
-        </v-avatar>
+        <v-avatar size="40"><v-img :src="companyImg" alt="Company logo" /></v-avatar>
         <span class="drawer-company">{{ company }}</span>
-        <v-btn icon @click.stop="mini = !mini">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
+        <v-btn icon @click.stop="mini = true"><v-icon>mdi-chevron-left</v-icon></v-btn>
       </div>
-      <!-- Drawer Header (mini) -->
+      <!-- Mini Header -->
       <div v-else class="drawer-header-mini">
-        <v-avatar size="40">
-          <v-img :src="company_img" alt="Company logo" />
-        </v-avatar>
+        <v-avatar size="40"><v-img :src="companyImg" alt="Company logo" /></v-avatar>
       </div>
 
       <v-divider />
 
+      <!-- Navigation Items -->
       <v-list dense nav>
         <v-list-item-group v-model="item" active-class="active-item">
           <v-list-item v-for="i in items" :key="i.text" @click="changePage(i.text)" class="drawer-item">
-            <v-list-item-icon>
-              <v-icon class="drawer-icon">{{ i.icon }}</v-icon>
-            </v-list-item-icon>
+            <v-list-item-icon><v-icon class="drawer-icon">{{ i.icon }}</v-icon></v-list-item-icon>
             <v-list-item-content v-if="!mini">
-              <v-list-item-title class="drawer-item-title">
-                {{ i.text }}
-              </v-list-item-title>
+              <v-list-item-title class="drawer-item-title">{{ i.text }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
 
-    <!-- Snack and Dialog -->
+    <!-- Snackbar -->
     <v-snackbar v-model="snack" :timeout="5000" :color="snackColor" location="top right">
       {{ snackText }}
     </v-snackbar>
 
+    <!-- Freeze Dialog -->
     <v-dialog v-model="freeze" persistent max-width="290">
       <v-card>
         <v-card-title class="text-h5">{{ freezeTitle }}</v-card-title>
@@ -114,129 +104,163 @@
 </template>
 
 <script>
+import { eventBus } from '@/eventBus';
+
 export default {
+  name: 'NavBar',
   data() {
     return {
       drawer: false,
       mini: true,
       item: 0,
       items: [{ text: 'POS', icon: 'mdi-network-pos' }],
-      page: '',
-      fav: true,
-      menu: false,
-      message: false,
-      hints: true,
-      menu_item: 0,
+      company: 'POS Awesome',
+      companyImg: '/assets/erpnext/images/erpnext-logo.svg',
+      posProfile: {},
+      lastInvoiceId: null,
       snack: false,
       snackColor: '',
       snackText: '',
-      company: 'POS Awesome',
-      company_img: '/assets/erpnext/images/erpnext-logo.svg',
-      pos_profile: '',
       freeze: false,
       freezeTitle: '',
-      freezeMsg: '',
-      last_invoice: '',
-      closeTimeout: null,
+      freezeMsg: ''
     };
   },
   created() {
-    // use boot company if available, otherwise keep default
-    const bootCompany = frappe.boot.user_info && frappe.boot.user_info.company;
+    const bootCompany = frappe?.boot?.user_info?.company;
     this.company = bootCompany || this.company;
+    console.log('Fetched company:', this.company);
 
-    // fetch company logo
-    if (this.company && this.company !== 'POS Awesome') {
+    if (this.company !== 'POS Awesome') {
       frappe.call({
         method: 'frappe.client.get',
         args: { doctype: 'Company', name: this.company },
         callback: r => {
-          if (r.message && r.message.company_logo) {
-            this.company_img = r.message.company_logo;
+          if (r.message?.company_logo) {
+            this.companyImg = r.message.company_logo;
           }
-        }
+        },
+        error: err => console.warn('Company lookup failed', err)
       });
     }
 
-    // existing event registrations
     this.$nextTick(() => {
-      this.eventBus.on('show_message', this.show_message);
-      this.eventBus.on('set_company', data => {
+      eventBus.on('show_message', this.showMessage);
+      eventBus.on('set_company', data => {
         this.company = data.name || this.company;
-        this.company_img = data.company_logo || this.company_img;
+        this.companyImg = data.company_logo || this.companyImg;
       });
-      this.eventBus.on('register_pos_profile', data => {
-        this.pos_profile = data.pos_profile;
-        const payments = { text: 'Payments', icon: 'mdi-cash-register' };
-        if (this.pos_profile.posa_use_pos_awesome_payments && this.items.length !== 2) {
-          this.items.push(payments);
+      eventBus.on('register_pos_profile', data => {
+        this.posProfile = data.pos_profile;
+        const paymentsItem = { text: 'Payments', icon: 'mdi-cash-register' };
+        if (this.posProfile.posa_use_pos_awesome_payments &&
+          !this.items.some(i => i.text === 'Payments')) {
+          this.items.push(paymentsItem);
         }
       });
-      this.eventBus.on('set_last_invoice', data => (this.last_invoice = data));
-      this.eventBus.on('freeze', data => { this.freeze = true; this.freezeTitle = data.title; this.freezeMsg = data.msg; });
-      this.eventBus.on('unfreeze', () => { this.freeze = false; this.freezeTitle = ''; this.freezeMsg = ''; });
+      eventBus.on('set_last_invoice', data => {
+        this.lastInvoiceId = data;
+      });
+      eventBus.on('freeze', data => {
+        this.freeze = true;
+        this.freezeTitle = data.title;
+        this.freezeMsg = data.msg;
+      });
+      eventBus.on('unfreeze', () => {
+        this.freeze = false;
+        this.freezeTitle = '';
+        this.freezeMsg = '';
+      });
     });
   },
   methods: {
+    handleNavClick() {
+      this.drawer = true;
+      this.mini = false;
+    },
+    handleMouseLeave() {
+      if (!this.drawer) return;
+      clearTimeout(this._closeTimeout);
+      this._closeTimeout = setTimeout(() => {
+        this.drawer = false;
+        this.mini = true;
+      }, 250);
+    },
     changePage(key) {
       this.$emit('changePage', key);
     },
-    go_desk() {
-      frappe.set_route('/'); location.reload();
+    goDesk() {
+      frappe.set_route('/');
+      location.reload();
     },
-    go_about() {
+    openCloseShift() {
+      this.eventBus.emit('open_closing_dialog');
+    },
+    printLastInvoice() {
+      if (!this.lastInvoiceId) return;
+      const pf = this.posProfile.print_format_for_online || this.posProfile.print_format;
+      const noLetterHead = this.posProfile.letter_head || 0;
+      const url = `${frappe.urllib.get_base_url()}/printview?doctype=Sales%20Invoice&name=${this.lastInvoiceId}` +
+        `&trigger_print=1&format=${pf}&no_letterhead=${noLetterHead}`;
+      const win = window.open(url, '_blank');
+      win.addEventListener('load', () => win.print(), { once: true });
+    },
+    goAbout() {
       frappe.call({
         method: 'posawesome.posawesome.api.posapp.get_app_info',
         callback: r => {
-          if (r.message && Array.isArray(r.message.apps)) {
+          if (Array.isArray(r.message.apps)) {
             let html = `
               <table style="width:100%; border-collapse:collapse; text-align:left;">
                 <thead>
-                  <tr>
-                    <th style="padding:8px;">${__('Application')}</th>
-                    <th style="padding:8px;">${__('Version')}</th>
-                  </tr>
-                </thead>
-                <tbody>
+                  <tr><th style="padding:8px;">${__('Application')}</th>
+                      <th style="padding:8px;">${__('Version')}</th></tr>
+                </thead><tbody>
             `;
             r.message.apps.forEach(app => {
               html += `
-                  <tr>
-                    <td style="padding:8px;"><strong>${app.app_name}</strong></td>
-                    <td style="padding:8px;">${app.installed_version}</td>
-                  </tr>
+                <tr>
+                  <td style="padding:8px;"><strong>${app.app_name}</strong></td>
+                  <td style="padding:8px;">${app.installed_version}</td>
+                </tr>
               `;
             });
-            html += `
-                </tbody>
-              </table>
-            `;
+            html += `</tbody></table>`;
 
-            frappe.msgprint({ title: __('Installed Applications'), indicator: 'blue', message: html });
+            frappe.msgprint({
+              title: __('Installed Applications'),
+              indicator: 'blue',
+              message: html
+            });
           }
         },
-        error: () => frappe.msgprint({ title: __('Error'), indicator: 'red', message: __('Failed to retrieve app info') })
+        error: () => frappe.msgprint({
+          title: __('Error'),
+          indicator: 'red',
+          message: __('Failed to retrieve app info')
+        })
       });
     },
-    close_shift_dialog() { this.eventBus.emit('open_closing_dialog'); },
-    show_message(data) { this.snack = true; this.snackColor = data.color; this.snackText = data.title; },
     logOut() {
-      let me = this; me.logged_out = true;
-      return frappe.call({ method: 'logout', callback: r => { if (!r.exc) { frappe.set_route('/app/home'); location.reload(); } } });
+      frappe.call({
+        method: 'logout',
+        callback: r => {
+          if (!r.exc) {
+            frappe.set_route('/app/home');
+            location.reload();
+          }
+        }
+      });
     },
-    print_last_invoice() {
-      if (!this.last_invoice) return;
-      const pf = this.pos_profile.print_format_for_online || this.pos_profile.print_format;
-      const lh = this.pos_profile.letter_head || 0;
-      const url = `${frappe.urllib.get_base_url()}/printview?doctype=Sales%20Invoice&name=${this.last_invoice}&trigger_print=1&format=${pf}&no_letterhead=${lh}`;
-      const pw = window.open(url, 'Print'); pw.addEventListener('load', () => pw.print(), true);
-    },
-    handleNavClick() { this.drawer = true; this.mini = false; },
-    handleMouseLeave() { if (!this.drawer) return; this.closeTimeout = setTimeout(() => { this.drawer = false; this.mini = true; }, 250); },
-    triggerNavClick() { if (!this.drawer) this.$refs.navIcon.$el.click(); }
+    showMessage(data) {
+      this.snack = true;
+      this.snackColor = data.color;
+      this.snackText = data.title;
+    }
   }
 };
 </script>
+
 
 <style scoped>
 .border-bottom {
