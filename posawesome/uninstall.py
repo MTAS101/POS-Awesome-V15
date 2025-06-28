cf0479266ce6af05074a0df931841f9837de3c97
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-06-28 22:03:36 +0500

    Fix uninstall filter processing

--- posawesome/uninstall.py
+++ posawesome/uninstall.py
@@ -9,18 +9,19 @@ def after_uninstall():
 def clear_custom_fields_and_properties():
     fixtures = frappe.get_hooks("fixtures", app_name="posawesome")
     for fixture in fixtures:
-        if fixture.get("doctype") == "Custom Field":
-            filters = fixture.get("filters")
-            if filters:
-                for filter in filters:
-                    frappe.db.delete("Custom Field", filter)
-                    print("Deleted Custom Fields: ", filter)
-        if fixture.get("doctype") == "Property Setter":
-            filters = fixture.get("filters")
-            if filters:
-                for filter in filters:
-                    frappe.db.delete("Property Setter", filter)
-                    print("Deleted Property Setter: ", filter)
+        doctype = fixture.get("doctype")
+        if not doctype:
+            continue
+
+        filters = fixture.get("filters") or []
+
+        for f in filters:
+            # fixture filters are defined as lists like ["name", "in", (<values>)]
+            if isinstance(f, (list, tuple)) and len(f) == 3:
+                f = {f[0]: (f[1], f[2])}
+
+            frappe.db.delete(doctype, f)
+            print(f"Deleted {doctype}: ", f)
 
     frappe.db.commit()
 
