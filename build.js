1804e2c26bfe09983b06754e3b0e3f20c28cfc3b
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-06 16:12:09 +0500

    build: switch to vite

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
 
 console.log('Installing dependencies...');
 execSync('yarn install', { stdio: 'inherit' });
 
 console.log('Building the application...');
-execSync('node esbuild --production --apps posawesome --run-build-command', { stdio: 'inherit' }); 
+execSync('vite build', { stdio: 'inherit' });
+
