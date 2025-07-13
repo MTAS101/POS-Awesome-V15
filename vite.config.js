42f5d8bc572b13d54cebfa87ab7cc8b5a85220ff
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-13 20:29:21 +0500

    Update worker imports and vite config (#1058)

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: '/assets/posawesome/',        // << no “js/” suffix
+  base: "/assets/posawesome/",
   plugins: [vue()],
   build: {
-    outDir: 'posawesome/public',      // write files at public root
-    assetsDir: '.',                   // keep in same folder
+    outDir: "posawesome/public",
+    assetsDir: ".",
     cssCodeSplit: true,
     rollupOptions: {
-      input: 'posawesome/public/posawesome.bundle.js',
+      input: "posawesome/public/posawesome.bundle.js",
       output: {
-        entryFileNames: 'posawesome.bundle.js',
-        assetFileNames: 'posawesome.css',
-        chunkFileNames: '[name]-[hash].js'
+        entryFileNames: "posawesome.bundle.js",
+        assetFileNames: "posawesome.css",
+        chunkFileNames: "[name]-[hash].js"
       }
     }
   }
