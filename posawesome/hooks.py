7717e7c95861ea4872b7bfc11d0024ca59499f4d
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-13 15:35:00 +0500

    Revert "fix service worker offline path (#1028)"
    This reverts commit 739eae7fa6afbc1e68300ae2a72c491aef1bfa8e.

--- posawesome/hooks.py
+++ posawesome/hooks.py
@@ -17,8 +17,8 @@ app_license = "GPLv3"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_js  = ["js/posawesome.bundle.js"]
-app_include_css = ["js/posawesome.css"]
+app_include_js  = ["posawesome.bundle.js"]
+app_include_css = ["/assets/posawesome/js/posawesome.css"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/posawesome/css/posawesome.css"
