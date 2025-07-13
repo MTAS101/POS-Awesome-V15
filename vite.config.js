b3aec2bea1076370740e3ddf63fd5d3e680fc6db
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-13 14:40:35 +0500

    fix worker and css paths

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,24 @@ import { resolve } from 'path';
 import frappeVueStyle from './frappe-vue-style.vite.js';
 
 export default defineConfig({
-        plugins: [frappeVueStyle(), vue()],
-       build: {
-               outDir: 'posawesome/public/js',
-               cssCodeSplit: true,
-               rollupOptions: {
-                        input: 'posawesome/public/js/posawesome.bundle.js',
-                        output: {
-                                entryFileNames: 'posawesome.bundle.js',
-                                assetFileNames: 'posawesome.css'
-                        }
-                }
-       },
-        resolve: {
-                alias: {
-                        "@": resolve(__dirname, "posawesome/public/js"),
-                },
-        },
+  plugins: [frappeVueStyle(), vue()],
+  base: '/assets/posawesome/js/',
+  build: {
+    outDir: 'posawesome/public/js',
+    assetsDir: '.',
+    cssCodeSplit: true,
+    emptyOutDir: false,
+    rollupOptions: {
+      input: 'posawesome/public/js/posawesome.bundle.js',
+      output: {
+        entryFileNames: 'posawesome.bundle.js',
+        assetFileNames: 'posawesome.css'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'posawesome/public/js'),
+    },
+  },
 });
