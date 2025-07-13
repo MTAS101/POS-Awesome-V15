957d69f9aca3428662ffaf4140cca5625ebb2221
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-13 15:04:57 +0500

    Fix asset paths and preserve sources

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import frappeVueStyle from './frappe-vue-style.vite.js';
 
 export default defineConfig({
-  plugins: [frappeVueStyle(), vue()],
   base: '/assets/posawesome/js/',
+  plugins: [vue()],
   build: {
     outDir: 'posawesome/public/js',
     assetsDir: '.',
     cssCodeSplit: true,
     emptyOutDir: false,
     rollupOptions: {
-      input: 'posawesome/public/js/posawesome.bundle.js',
+      input: resolve(__dirname, 'posawesome/public/js/posawesome.bundle.js'),
       output: {
         entryFileNames: 'posawesome.bundle.js',
-        assetFileNames: 'posawesome.css'
+        assetFileNames: 'posawesome.css',
+        chunkFileNames: '[name]-[hash].js'
       }
     }
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'posawesome/public/js'),
-    },
-  },
+      '@': resolve(__dirname, 'posawesome/public/js')
+    }
+  }
 });
