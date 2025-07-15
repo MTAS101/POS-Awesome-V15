f7de8de647cbf6bb47c04d6a1113d5fc3a4eba26
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-15 10:17:04 +0500

    fix barcode UOM

--- posawesome/public/js/posapp/components/pos/invoiceItemMethods.js
+++ posawesome/public/js/posapp/components/pos/invoiceItemMethods.js
@@ -175,8 +175,8 @@ export default {
 			new_item.item_uoms.push({ uom: new_item.stock_uom, conversion_factor: 1 });
 		}
 		new_item.actual_batch_qty = "";
-		new_item.batch_no_expiry_date = item.batch_no_expiry_date || null;
-		new_item.conversion_factor = 1;
+                new_item.batch_no_expiry_date = item.batch_no_expiry_date || null;
+                new_item.conversion_factor = item.conversion_factor || 1;
 		new_item.posa_offers = JSON.stringify([]);
 		new_item.posa_offer_applied = 0;
 		new_item.posa_is_offer = item.posa_is_offer;
