72a31e34c6421230077102267902dfca61b9d853
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-16 11:23:40 +0500

    fix module specifier

--- posawesome/public/js/offline/core.js
+++ posawesome/public/js/offline/core.js
@@ -1,4 +1,4 @@
-import Dexie from "dexie";
+import Dexie from "../libs/dexie.mjs";
 import { withWriteLock } from "./db-utils.js";
 
 // --- Dexie initialization ---------------------------------------------------
