b02a7bc41d71eaf261cf72b80b23ba2c787f786d
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-16 17:01:27 +0500

    Fix Dexie module imports (#1105)

--- posawesome/public/js/offline.js
+++ posawesome/public/js/offline.js
@@ -1,4 +1,4 @@
-import Dexie from "dexie";
+import Dexie from "./libs/dexie.min.js";
 
 // --- Dexie initialization ---------------------------------------------------
 const db = new Dexie("posawesome_offline");
