96bf42e322c119036427c171e19b16456d1c6c41
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-19 21:26:01 +0500

    fix variant rate fetch and eslint (#1251)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,14 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 import pluginVuetify from "eslint-plugin-vuetify";
+import vueParser from "vue-eslint-parser";
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
     files: ["**/*.{js,mjs,cjs,vue}"],
     languageOptions: {
+      parser: vueParser,
       parserOptions: { ecmaVersion: 2020, sourceType: "module" },
       globals: globals.browser,
     },
