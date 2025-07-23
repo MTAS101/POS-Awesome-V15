22c30f2c66c661f9978eebdcc1abe875a571846b
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-23 23:52:52 +0500

    fix: ensure only one item expands

--- posawesome/public/js/posapp/components/pos/Invoice.vue
+++ posawesome/public/js/posapp/components/pos/Invoice.vue
@@ -183,7 +183,7 @@
 						ref="itemsTable"
 						:headers="items_headers"
 						:items="items"
-						:expanded="expanded"
+						v-model:expanded="expanded"
 						:itemsPerPage="itemsPerPage"
 						:itemSearch="itemSearch"
 						:pos_profile="pos_profile"
@@ -207,7 +207,6 @@
 						:addOne="add_one"
 						:toggleOffer="toggleOffer"
 						:changePriceListRate="change_price_list_rate"
-						@update:expanded="expanded = $event"
 						@reorder-items="handleItemReorder"
 						@add-item-from-drag="handleItemDrop"
 						@show-drop-feedback="showDropFeedback"
@@ -255,7 +254,7 @@ import InvoiceSummary from "./InvoiceSummary.vue";
 import ItemsTable from "./ItemsTable.vue";
 import invoiceItemMethods from "./invoiceItemMethods";
 import invoiceComputed from "./invoiceComputed";
-import invoiceWatchers from "./invoiceWatchers"
+import invoiceWatchers from "./invoiceWatchers";
 import offerMethods from "./invoiceOfferMethods";
 import shortcutMethods from "./invoiceShortcuts";
 import { isOffline, saveCustomerBalance, getCachedCustomerBalance } from "../../../offline";
@@ -303,7 +302,7 @@ export default {
 			selected_currency: "", // Currently selected currency
 			exchange_rate: 1, // Current exchange rate
 			conversion_rate: 1, // Currency to company rate
-                        exchange_rate_date: frappe.datetime.nowdate(), // Date of fetched exchange rate
+			exchange_rate_date: frappe.datetime.nowdate(), // Date of fetched exchange rate
 			company: null, // Company doc with default currency
 			available_currencies: [], // List of available currencies
 			price_lists: [], // Available selling price lists
@@ -1144,7 +1143,7 @@ export default {
 		this.eventBus.on("reset_posting_date", () => {
 			this.posting_date = frappe.datetime.nowdate();
 		});
-               this.eventBus.on("calc_uom", this.calc_uom);
+		this.eventBus.on("calc_uom", this.calc_uom);
 		this.eventBus.on("item-drag-start", (item) => {
 			this.showDropFeedback(true);
 		});
--- posawesome/public/js/posapp/components/pos/ItemsTable.vue
+++ posawesome/public/js/posapp/components/pos/ItemsTable.vue
@@ -11,7 +11,7 @@
 			:headers="headers"
 			:items="items"
 			:theme="$theme.current"
-			:expanded="expanded"
+			v-model:expanded="expanded"
 			show-expand
 			item-value="posa_row_id"
 			class="modern-items-table elevation-2"
@@ -21,7 +21,6 @@
 			hide-default-footer
 			:single-expand="true"
 			:header-props="headerProps"
-                        @update:expanded="val => $emit('update:expanded', val.map(v => typeof v === 'object' ? v.posa_row_id : v))"
 			:search="itemSearch"
 		>
 			<!-- Quantity column -->
@@ -513,7 +512,7 @@
 </template>
 
 <script>
-import _ from 'lodash';
+import _ from "lodash";
 export default {
 	name: "ItemsTable",
 	props: {
@@ -612,10 +611,10 @@ export default {
 		addItem(newItem) {
 			// Find a matching item (by item_code, uom, and rate)
 			const match = this.items.find(
-				item =>
+				(item) =>
 					item.item_code === newItem.item_code &&
 					item.uom === newItem.uom &&
-					item.rate === newItem.rate
+					item.rate === newItem.rate,
 			);
 			if (match) {
 				// If found, increment quantity
@@ -626,7 +625,7 @@ export default {
 				this.items.push({ ...newItem });
 			}
 		},
-		addItemDebounced: _.debounce(function(item) {
+		addItemDebounced: _.debounce(function (item) {
 			this.addItem(item);
 		}, 50),
 	},
