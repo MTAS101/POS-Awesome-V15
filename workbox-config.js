2e9e9849c291732f9b9b3d167556115fe95a3e99
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-23 09:42:37 +0500

    feat: add Workbox service worker (#1389)

---
+++ workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	globDirectory: "posawesome/www",
+	globPatterns: ["**/*.{js,css,html,png,svg,json}"],
+	swSrc: "posawesome/www/sw-source.js",
+	swDest: "posawesome/www/sw.js",
+};
