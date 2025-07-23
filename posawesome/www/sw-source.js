08dd7937ae249303a96cdbacaa1cf56e92a34e8a
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-23 09:42:06 +0500

    feat: add Workbox service worker

--- posawesome/www/sw-source.js
+++ posawesome/www/sw-source.js
@@ -2,7 +2,7 @@ import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute, setDefaultHandler } from "workbox-routing";
 import { NetworkFirst, StaleWhileRevalidate } from "workbox-strategies";
 
-precacheAndRoute([{"revision":"d882d32fc586c1c56dfb0ff4c9ca9ff9","url":"manifest.json"},{"revision":"0b2f8fe6908c2bc1aa82bc5593f15bd1","url":"offline.html"},{"revision":"b93b0f3bad1848ad38b2aa6cab11265e","url":"sw-old.js"}]);
+precacheAndRoute(self.__WB_MANIFEST);
 
 // Cache application pages
 registerRoute(({ request }) => request.mode === "navigate", new NetworkFirst({ cacheName: "pages-cache" }));
