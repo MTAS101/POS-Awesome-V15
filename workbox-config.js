2f0d6a3eddacc56598a8ad878a87e6084650989d
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-23 10:26:31 +0500

    Revert "fix: precache app resources for offline (#1395)"
    This reverts commit 0e184ef1b9f770ac396c131e4bb26e0103f60009.

--- workbox-config.js
+++ workbox-config.js
@@ -1,19 +1,6 @@
 module.exports = {
-        globDirectory: "posawesome/www",
-        globPatterns: ["**/*.{js,css,html,png,svg,json}"],
-        swSrc: "posawesome/www/sw-source.js",
-        swDest: "posawesome/www/sw.js",
-        additionalManifestEntries: [
-                { url: "/app/posapp", revision: null },
-                {
-                        url: "/assets/posawesome/js/posawesome.bundle.js",
-                        revision: null,
-                },
-                { url: "/assets/posawesome/js/offline/index.js", revision: null },
-                {
-                        url: "/assets/posawesome/js/posapp/workers/itemWorker.js",
-                        revision: null,
-                },
-                { url: "/assets/posawesome/js/libs/dexie.min.js", revision: null },
-        ],
+	globDirectory: "posawesome/www",
+	globPatterns: ["**/*.{js,css,html,png,svg,json}"],
+	swSrc: "posawesome/www/sw-source.js",
+	swDest: "posawesome/www/sw.js",
 };
