70b75ad0874f9a7ea35ef5562073f61cdc8bdc91
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-23 10:15:42 +0500

    fix: precache app resources for offline

--- posawesome/www/sw.js
+++ posawesome/www/sw.js
@@ -7,7 +7,7 @@ const { precacheAndRoute } = precaching;
 const { registerRoute, setDefaultHandler } = routing;
 const { NetworkFirst, StaleWhileRevalidate } = strategies;
 
-precacheAndRoute([{"revision":"d882d32fc586c1c56dfb0ff4c9ca9ff9","url":"manifest.json"},{"revision":"de35420b35ebe9d252e43e4802b2287b","url":"offline.html"},{"revision":"b93b0f3bad1848ad38b2aa6cab11265e","url":"sw-old.js"}]);
+precacheAndRoute([{"revision":"d882d32fc586c1c56dfb0ff4c9ca9ff9","url":"manifest.json"},{"revision":"de35420b35ebe9d252e43e4802b2287b","url":"offline.html"},{"revision":"b93b0f3bad1848ad38b2aa6cab11265e","url":"sw-old.js"},{"revision":null,"url":"/app/posapp"},{"revision":null,"url":"/assets/posawesome/js/posawesome.bundle.js"},{"revision":null,"url":"/assets/posawesome/js/offline/index.js"},{"revision":null,"url":"/assets/posawesome/js/posapp/workers/itemWorker.js"},{"revision":null,"url":"/assets/posawesome/js/libs/dexie.min.js"}]);
 
 // Cache application pages
 registerRoute(({ request }) => request.mode === "navigate", new NetworkFirst({ cacheName: "pages-cache" }));
