e59a32a352951330d2201dd952f9eceea2d2e958
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-24 09:16:32 +0500

    feat: switch build to vite

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [vue()],
-	build: {
-		target: "esnext",
-		lib: {
-			entry: resolve(__dirname, "posawesome/public/js/posawesome.bundle.js"),
-			name: "PosAwesome",
-			fileName: "posawesome",
-		},
-		outDir: "posawesome/public/dist/js",
-		emptyOutDir: true,
-		rollupOptions: {
-			external: ["socket.io-client"],
-			output: {
-				globals: {
-					"socket.io-client": "io",
-				},
-			},
-		},
+       plugins: [vue(), vuetify({ autoImport: true })],
+        build: {
+                target: "esnext",
+               lib: {
+                        entry: resolve(__dirname, "posawesome/public/js/posawesome.bundle.js"),
+                        name: "PosAwesome",
+                        fileName: () => "posawesome.bundle.js",
+                        formats: ["es"],
+                },
+                outDir: "posawesome/public/dist/js",
+                emptyOutDir: true,
+               rollupOptions: {
+                        external: ["socket.io-client"],
+                        output: {
+                                inlineDynamicImports: true,
+                                globals: {
+                                        "socket.io-client": "io",
+                                },
+                        },
+                },
 	},
 	resolve: {
 		alias: {
