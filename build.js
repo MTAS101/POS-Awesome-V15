41af8cea9ce7f1a9abfe9444524f17b1e2dbb472
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-24 10:40:42 +0500

    chore: remove legacy build.js to stop esbuild fallback

--- build.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require("child_process");
-
-console.log("Installing dependencies...");
-execSync("yarn install", { stdio: "inherit" });
-
-console.log("Building the application with Vite...");
-execSync("npx vite build", { stdio: "inherit" });
