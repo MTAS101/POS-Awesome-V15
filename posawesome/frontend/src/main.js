5fe4c6cef88e9723c6709a8b75c9c2f20925da05
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-07-25 00:20:52 +0500

    fix module resolution (#1459)

--- posawesome/frontend/src/main.js
+++ posawesome/frontend/src/main.js
@@ -1,13 +1,10 @@
-const script = document.createElement("script");
-script.src = "/assets/posawesome/js/posawesome.bundle.js";
-script.type = "module";
-document.head.appendChild(script);
+import "posapp/posapp.js";
 
 // Initialize POS Awesome Vue app if available
 if (window.frappe && frappe.PosApp && frappe.PosApp.posapp) {
   frappe.ready(() => {
     if (!document.getElementById("app")) return;
-    const page = { page: { }, $el: [document.getElementById("app")] };
+    const page = { page: {}, $el: [document.getElementById("app")] };
     new frappe.PosApp.posapp(page);
   });
 }
