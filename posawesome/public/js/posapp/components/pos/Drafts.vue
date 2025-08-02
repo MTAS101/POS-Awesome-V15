ce892df2d611d0d26251b0d8a0b3fe754b556e73
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-08-02 11:11:44 +0500

    fix: use reactive vuetify theme

--- posawesome/public/js/posapp/components/pos/Drafts.vue
+++ posawesome/public/js/posapp/components/pos/Drafts.vue
@@ -4,7 +4,7 @@
 			<!-- <template v-slot:activator="{ on, attrs }">
         <v-btn color="primary" theme="dark" v-bind="attrs" v-on="on">Open Dialog</v-btn>
       </template>-->
-			<v-card variant="flat" color="white">
+			<v-card variant="flat" :color="isDarkTheme ? $vuetify.theme.themes.dark.colors.surface : 'white'">
 				<v-card-title>
 					<span class="text-h5 text-primary">{{ __("Load Sales Invoice") }}</span>
 				</v-card-title>
@@ -20,6 +20,7 @@
 									:items="dialog_data"
 									item-value="name"
 									class="elevation-1"
+									:theme="isDarkTheme ? 'dark' : 'light'"
 									show-select
 									v-model="selected"
 									select-strategy="single"
@@ -48,6 +49,7 @@
 </template>
 
 <script>
+/* global __ */
 import format from "../../format";
 export default {
 	// props: ["draftsDialog"],
@@ -90,6 +92,11 @@ export default {
 			},
 		],
 	}),
+	computed: {
+		isDarkTheme() {
+			return this.$vuetify.theme.current.value.dark;
+		},
+	},
 	watch: {},
 	methods: {
 		close_dialog() {
