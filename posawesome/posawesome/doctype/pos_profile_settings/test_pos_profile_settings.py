e78a3b26b3b683d8f52dc46cebfcc9db840f8e1a
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-08-03 01:29:37 +0500

    Revert "Revert "Revert "Refactor custom fields (#1594)" (#1596)" (#1597)"
    This reverts commit b543f6020384e0dc3ca1fc9f5fb55cd79bda4fa6.

--- posawesome/posawesome/doctype/pos_profile_settings/test_pos_profile_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, POS Awesome and contributors
-# For license information, please see license.txt
-
-import unittest
-import frappe
-
-
-class TestPOSProfileSettings(unittest.TestCase):
-	pass 
