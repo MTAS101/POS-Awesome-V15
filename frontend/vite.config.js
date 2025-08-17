c8ff9e63cc11d6b2bb93a71fd9f04fa55ba78af9
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-08-17 16:27:29 +0500

    refactor: move js sources to frontend and update build

---
+++ frontend/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import frappeVueStyle from "../frappe-vue-style";
+
+export default defineConfig({
+  plugins: [frappeVueStyle(), vue()],
+  build: {
+    target: "esnext",
+    lib: {
+      entry: resolve(__dirname, "src/posawesome.bundle.js"),
+      name: "PosAwesome",
+      fileName: "posawesome",
+    },
+    outDir: "../posawesome/public/dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ["socket.io-client"],
+      output: {
+        globals: {
+          "socket.io-client": "io",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  define: {
+    "process.env.NODE_ENV": '"production"',
+    process: '{"env":{}}',
+  },
+});
