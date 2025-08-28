8335d3ec1aed5c0bb5a88cd01470c1fc2c5e89fe
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-08-28 19:55:52 +0500

    refactor: use incremental progress bar overlay

--- frontend/src/posapp/components/pos/LoadingOverlay.vue
+++ frontend/src/posapp/components/pos/LoadingOverlay.vue
@@ -4,7 +4,13 @@
 		class="d-flex flex-column align-center justify-center fancy-overlay"
 		contained
 	>
-		<v-progress-circular indeterminate size="64" width="6" color="primary" />
+		<v-progress-linear
+			:model-value="progress"
+			height="8"
+			color="primary"
+			rounded
+			class="elegant-progress"
+		/>
 		<div class="mt-4 text-subtitle-1">{{ message }}</div>
 	</v-overlay>
 </template>
@@ -22,11 +28,62 @@ export default {
 			default: "",
 		},
 	},
+	data() {
+		return {
+			progress: 0,
+			intervalId: null,
+		};
+	},
+	watch: {
+		loading(val) {
+			if (val) {
+				this.startProgress();
+			} else {
+				this.finishProgress();
+			}
+		},
+	},
+	mounted() {
+		if (this.loading) {
+			this.startProgress();
+		}
+	},
+	beforeUnmount() {
+		this.clearTimer();
+	},
+	methods: {
+		startProgress() {
+			this.clearTimer();
+			this.progress = 0;
+			this.intervalId = setInterval(() => {
+				if (this.progress < 90) {
+					this.progress += 5;
+				}
+			}, 200);
+		},
+		finishProgress() {
+			this.clearTimer();
+			this.progress = 100;
+			setTimeout(() => {
+				this.progress = 0;
+			}, 300);
+		},
+		clearTimer() {
+			if (this.intervalId) {
+				clearInterval(this.intervalId);
+				this.intervalId = null;
+			}
+		},
+	},
 };
 </script>
 
 <style scoped>
 .fancy-overlay {
 	backdrop-filter: blur(2px);
+}
+.elegant-progress {
+	width: 60%;
+	max-width: 320px;
 }
 </style>
