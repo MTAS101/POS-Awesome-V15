b24d8fc711c54cc4c3ae2f726bb4a7af05c18715
Abdul Manan <135047084+defendicon@users.noreply.github.com>
2025-08-29 13:18:11 +0500

    fix: apply brand offers across all items

--- frontend/src/posapp/components/pos/invoiceOfferMethods.js
+++ frontend/src/posapp/components/pos/invoiceOfferMethods.js
@@ -158,7 +158,7 @@ export default {
 						if (
 							offer.offer === "Item Price" &&
 							item.posa_offer_applied &&
-							!this.checkOfferIsAppley(item, offer)
+							this.checkOfferIsAppley(item, offer)
 						) {
 							return;
 						}
@@ -190,7 +190,7 @@ export default {
 						if (
 							offer.offer === "Item Price" &&
 							item.posa_offer_applied &&
-							!this.checkOfferIsAppley(item, offer)
+							this.checkOfferIsAppley(item, offer)
 						) {
 							return;
 						}
@@ -225,7 +225,7 @@ export default {
 						if (
 							offer.offer === "Item Price" &&
 							item.posa_offer_applied &&
-							!this.checkOfferIsAppley(item, offer)
+							this.checkOfferIsAppley(item, offer)
 						) {
 							return;
 						}
